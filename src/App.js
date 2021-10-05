87274267b541ee95884185ea134c96cb83767464
Christian <christian.mf22@gmail.com>
2021-10-05 11:38:53 -0300

    Created light and dark theme

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { AppContainer, MainSection } from "./components/app.style"
+import { Button } from "./components/button.style"
+import { ThemeProvider } from "styled-components"
+import { useCallback, useState } from "react"
+import { themes } from "./themes"
+
+function App() {
+  const [currentTheme, setCurrentTheme] = useState("light")
+
+  const getOpositeTheme = useCallback(
+    () => (currentTheme === "light" ? "dark" : "light"),
+    [currentTheme]
+  )
+
+  return (
+    <ThemeProvider theme={themes[currentTheme]}>
+      <AppContainer>
+        <MainSection>
+          <h1>You are in {currentTheme} mode</h1>
+          <Button onClick={() => setCurrentTheme(getOpositeTheme())}>
+            switch to {getOpositeTheme()} mode
+          </Button>
+        </MainSection>
+      </AppContainer>
+    </ThemeProvider>
+  )
+}
+
+export default App
